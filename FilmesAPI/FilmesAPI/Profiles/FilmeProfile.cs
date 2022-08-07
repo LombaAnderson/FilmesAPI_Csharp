4c2deacd112ba7339676fb6b7d09fbde154ea707
Anderson Lomba <andersonlomba172@gmail.com>
2022-08-07 14:02:18 -0300

    First commit

---
+++ FilmesAPI/FilmesAPI/Profiles/FilmeProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Profiles
+{
+    public class FilmeProfile : Profile
+    {
+
+        public FilmeProfile ()
+        {
+            CreateMap<CreateFilmeDto, Filme>();
+            CreateMap<Filme, CreateFilmeDto>();
+            CreateMap<UpdateFilmeDto, Filme>();
+
+        }
+    }
+}
